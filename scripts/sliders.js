2e8f5efe85b1dfb8acea2f141139586db0e5c906
Daniel Storm <danielstorm@MacBook-Pro-17.local>
2019-04-09 11:19:32 -0500

    stuff

--- scripts/sliders.js
+++ scripts/sliders.js
@@ -4,16 +4,19 @@ var slider3 = document.getElementById("slideRange3");
 var output1 = document.getElementById("value1");
 var output2 = document.getElementById("value2");
 var output3 = document.getElementById("value3");
-output1.innerHTML = slider1.value1; // Display the default slider value
-output1.innerHTML = slider2.value2;
-output1.innerHTML = slider3.value3;
-// Update the current slider value (each time you drag the slider handle)
+
+output1.innerHTML = slider1.value;
+output2.innerHTML = slider2.value;
+output3.innerHTML = slider3.value;
+
 slider1.oninput = function() {
-  output1.innerHTML = this.value;
+    output1.innerHTML = this.value;
 }
+
 slider2.oninput = function() {
     output2.innerHTML = this.value;
-  }
+}
+
 slider3.oninput = function() {
     output3.innerHTML = this.value;
-}  
+}               
